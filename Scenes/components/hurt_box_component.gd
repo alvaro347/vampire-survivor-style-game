1d5049cfc55756e3eb2f29da7666f14ef3ad7d39
Alvaro <alvaro347@gmail.com>
2024-10-04 23:11:21 +0100

    added damage text to enemies

--- Scenes/components/hurt_box_component.gd
+++ Scenes/components/hurt_box_component.gd
@@ -4,6 +4,7 @@ class_name HurtBoxComponent
 
 @export var health_component: Node
 
+var floating_text_scene = preload("res://Scenes/ui/floating_text.tscn")
 
 
 func _ready() -> void:
@@ -18,3 +19,9 @@ func on_area_entered(other_area: Area2D) -> void:
 
 	var hitbox_component: HitBoxComponent = other_area
 	health_component.take_damage(hitbox_component.damage)
+	
+	var floating_text: Node2D = floating_text_scene.instantiate()
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
