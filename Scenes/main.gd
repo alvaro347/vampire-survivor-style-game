86032d42174b0888b7c3d0f9101f4437de2dce70
Alvaro <alvaro347@gmail.com>
2024-10-13 23:32:50 +0100

    pause menu with options and back button

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,9 +3,17 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var pause_menu_scene = preload("res://Scenes/ui/pause_menu.tscn")
+
 
 func _ready() -> void:
 	%Player.health_component.died.connect(on_player_died)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 # In this case we set defeat after instantiating since it's not yet in the tree
