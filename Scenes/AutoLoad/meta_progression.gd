7396f1d4a8aa185df82698345bff355984d7aaa1
Alvaro <alvaro347@gmail.com>
2024-10-16 22:03:28 +0100

    save data file creation, progression and death

--- Scenes/AutoLoad/meta_progression.gd
+++ Scenes/AutoLoad/meta_progression.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+const SAVE_FILE_PATH="user://game.save"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {}
@@ -9,7 +11,20 @@ var save_data: Dictionary = {
 
 func _ready():
 	GameEvents.expirience_vial_collected.connect(on_exprience_collected)
-	add_meta_upgrade(load("res://resources/meta_upgrades/exprience_gain.tres"))
+	# add_meta_upgrade(load("res://resources/meta_upgrades/exprience_gain.tres"))
+	load_save_file()
+
+
+func load_save_file():
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
 
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
